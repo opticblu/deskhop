bbf2aaa8d0285221fbcd6463c078a215bfd61f9e
Philip <opticblu@hotmail.com>
2025-04-19 19:30:23 -0500

    Update keyboard.c

--- src/keyboard.c
+++ src/keyboard.c
@@ -17,7 +17,7 @@
 
 hotkey_combo_t hotkeys[] = {
     /* Main keyboard switching hotkey */
-    {.modifier       = HOTKEY_MODIFIER,
+    {.modifier       = 0,
      .keys           = {HOTKEY_TOGGLE},
      .key_count      = 1,
      .pass_to_os     = false,
